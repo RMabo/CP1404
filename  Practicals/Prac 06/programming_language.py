276d4ca535dafb0c40b7badd5e927b7e5b94a27e
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2021-11-09 11:56:47 +1000

    add __init__ function add classes to programming_language add is_dynamic function

---
+++  Practicals/Prac 06/programming_language.py
@@ -0,0 +1,22 @@
+class ProgrammingLanguage():
+    """Represents a Programming Language Object"""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise the ProgrammingLanguage instance.
+
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return '{}, {} Typing, Reflection={}, First appeared in {}'.format(self.name, self.typing,
+                                                                           str(self.reflection), str(self.year))
+
+    def is_dynamic(self):
+        """Checks if dynamic and returns Boolean value"""
+        if self.typing == 'Dynamic':
+            return True
+        else:
+            return True
