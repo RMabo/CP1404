2c96be6687e83f6aaf8133f41a4c4764dc155d11
Roderick Mabo <roderick.mabo@my.jcu.edu.au>
2021-11-10 16:14:24 +1000

    add os.rename(filename, get_fixed_filename(filename)) add shutil.move(filename, 'temp/' + get_fixed_filename(filename)) add loop to rename file

---
+++  Practicals/Prac 08/unreliable_car.py
@@ -0,0 +1,13 @@
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    """UnreliableCars Test"""
+
+    Ford = UnreliableCar("BAD", 100, 50)
+    Ford.add_fuel = (50)
+    Ford.drive = (100)
+    print(Ford)
+
+
+main()
